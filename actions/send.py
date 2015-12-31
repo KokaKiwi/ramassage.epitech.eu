311cc4840d6cda0add30ca38462afbfe3cca44a3
Steven MARTINS <steven.martins.fr@gmail.com>
2015-12-31 16:40:57 +0100

    Actions.send

---
+++ actions/send.py
@@ -0,0 +1,22 @@
+__author__ = 'steven'
+
+from mixins.scm import FsMixin
+from exceptions import FileMissing
+import logging
+import os
+import config
+
+class SendFile(FsMixin):
+    def __init__(self, project):
+        self._project = project
+
+    def path(self):
+        archive_name = os.path.join(config.ARCHIVE_DIR, self._cleanfilename("%s-%s" % (
+            self._project["title"], self._project["city"])))
+        filename = self._last_version("%s.zip" % (archive_name), with_extension=True)
+        logging.warning(filename)
+        if not os.path.isfile("%s.zip" % (archive_name)):
+            raise FileMissing()
+        filename = os.path.basename(filename)
+        filepath = config.ARCHIVE_DIR
+        return filepath, filename
